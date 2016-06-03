c0bfb70c5ae885b2e4508088b20d59d607e30d9e
Oscar Guadalaja6 <OSCAR@oscar-2.local>
2016-06-02 21:11:07 -0700

    Initial Commit

---
+++ VelocimetroDigital/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VelocimetroDigital
+//
+//  Created by OSCAR ATANACIO on 02/06/16.
+//  Copyright © 2016 OSCAR ATANACIO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
